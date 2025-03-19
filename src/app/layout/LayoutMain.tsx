a6ac647cba8872c3f426723d13c9db5b3fc2286b
Võ Phước Thạnh <phuocthanh2k03@gmail.com>
2025-03-19 13:46:18 +0700

    remove children

--- src/app/layout/LayoutMain.tsx
+++ src/app/layout/LayoutMain.tsx
@@ -10,9 +10,8 @@ const LayoutMain = ({ children }: ILayoutMainProps) => {
     <div className='flex flex-col h-screen overflow-hidden'>
       <div className='flex flex-1 overflow-hidden'>
         <Sidebar />
-        <main className='flex-1 overflow-auto px-2 py-2 bg-[#FFFFFF]'>{children}</main>
         <main className='flex-1 overflow-auto bg-[#F4F4F4]'>
-          <div className='w-full h-16 bg-[#FCFCFC]  px-4 fixed z-50  '>
+          <div className='w-full h-16 bg-[#FCFCFC]  px-4 fixed z-50'>
             <Avatar className='absolute top-4 left-[97rem]'>
               <AvatarImage
                 src='https://thuthuatnhanh.com/wp-content/uploads/2022/04/Hinh-nen-Songoku-4K-dep-nhat.jpg'
